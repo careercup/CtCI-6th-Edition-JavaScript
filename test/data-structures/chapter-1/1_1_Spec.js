4533fbffd566f529b9f0002aca234583f2522e00
nem035 <n.stojanovic035@gmail.com>
2016-12-28 01:13:55 -0500

    Additional implementations and refactored testing for Chapter 1.1

--- test/data-structures/chapter-1/1_1_Spec.js
+++ test/data-structures/chapter-1/1_1_Spec.js
@@ -1,18 +1,29 @@
 require('../../test_helper');
-describe('1.1 #hasUniqueChars', function () {
+
+function runTest(methodName) {
   var str1, str2;
   beforeEach(function () {
     str1 = 'abcdef';
-    str2 = 'qwertyuiopasdfghjkl;\'zxcvbnm,.';
+    str2 = 'qwertyuiopasdfghjkl;zxcvbnm,.';
     str3 = 'abcdefa';
   });
   it('returns true if string is all unique chars', function () {
-    expect(Strings_1_1.isUnique(str1)).to.be.true;
+    expect(Strings_1_1[methodName](str1)).to.be.true;
   });
-  it('returns true if string is all unique chars', function () {
-    expect(Strings_1_1.isUnique(str2)).to.be.true;
+  it('returns false if string is all unique chars', function () {
+    expect(Strings_1_1[methodName](str2)).to.be.true;
   });
   it('returns false if string contains duplicate chars', function () {
-    expect(Strings_1_1.isUnique(str3)).to.be.false;
+    expect(Strings_1_1[methodName](str3)).to.be.false;
   });
+}
+
+describe('Strings_1_1', function () {
+
+  var methodCount = 4;
+  for (var i = 1; i <= methodCount; i++) {
+    describe('hasUniqueChars' + i, function () {
+      runTest('hasUniqueChars' + i);
+    });
+  }
 });
