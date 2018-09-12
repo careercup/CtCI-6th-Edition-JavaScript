4fa640aedeb6b4478e70320af2f5fcf269c39aea
Daniel Biales <daniel.biales@libertymutual.com>
2018-09-12 15:14:41 -0400

    creating recursive examples of exercise 2.5 and the added the forward solution

--- chapter02/2.5 - Sum Lists/sumLists - recursion.js
+++ chapter02/2.5 - Sum Lists/sumLists - recursion.js
@@ -1,5 +1,5 @@
-const LinkedList = require('./../util/LinkedList');
-const printList = require('./../util/printList');
+const LinkedList = require('./../util/LinkedList')
+const printList = require('./../util/printList')
 
 function sumLinkedLists(node1,node2,carry=0){
   if(!node1&&!node2&&carry===0){
@@ -16,18 +16,18 @@ function sumLinkedLists(node1,node2,carry=0){
 // Input: (7 -> 1 -> 6) + (5 -> 9 -> 2). this case refers to 617 + 295
 // Output: 2 -> 1 -> 9. the answer refers to 912
 
-var a = new LinkedList(7);
-var b = new LinkedList(1);
-var c = new LinkedList(6);
+var a = new LinkedList(7)
+var b = new LinkedList(1)
+var c = new LinkedList(6)
 
-a.next = b;
-b.next = c;
+a.next = b
+b.next = c
 
-var d = new LinkedList(5);
-var e = new LinkedList(9);
-var f = new LinkedList(2);
+var d = new LinkedList(5)
+var e = new LinkedList(9)
+var f = new LinkedList(2)
 
-d.next = e;
-e.next = f;
+d.next = e
+e.next = f
 
 printList(sumLinkedLists(a,d))
