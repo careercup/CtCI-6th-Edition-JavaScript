7e489ddeb0929aa3b69391779e8bb6e8f96a6ac5
Sai Charan R Patlolla <scrpatlolla@users.noreply.github.com>
2019-08-03 13:38:12 -0400

    Update urlify.js

--- chapter01/1.3 - URLify/urlify.js
+++ chapter01/1.3 - URLify/urlify.js
@@ -1,24 +1,7 @@
-var urlify = function(str, length) {
-  // have a pointer to check from start to end
-  var strArr = str.split('');
-  var pointer = 0;
-  while (pointer < str.length) {
-    if (strArr[pointer] === ' ') {
-      // *** needs more work here, a little wierd
-      // not handling trailing spaces properly
-      for (var i = str.length - 1; i > pointer + 3; i--) {
-        strArr[i] = str[i - 2];
-      }
-      strArr[pointer] = '%';
-      strArr[pointer+1] = '2';
-      strArr[pointer+2] = '0';
-      console.log(strArr, strArr.length);
-    } 
-    pointer++;
-  }
-  // if character is a space, move remainder chars by two
-  // replace following three chars with '%20'
-  return strArr.join('');
-};
+function urlIfy(val) {
+  const removeTrails = val.trim();
+  return encodeURI(removeTrails);
+}
 
-console.log(urlify('Mr John Smith    ', 13), 'Mr%20John%20Smith');
+
+console.log(urlIfy("Mr John Smith  "))
