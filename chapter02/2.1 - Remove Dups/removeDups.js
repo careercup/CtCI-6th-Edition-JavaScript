3b696f0e3b11f8248598edddd2e27837c43f80cf
Anthony Nandaa <profnandaa@gmail.com>
2020-03-28 10:43:57 +0300

    fix(2.1): redo with proper linked list
    redid 2.1 using a properly defined linked list

--- chapter02/2.1 - Remove Dups/removeDups.js
+++ chapter02/2.1 - Remove Dups/removeDups.js
@@ -1,82 +1,38 @@
-/* CLASS */
-var LinkedList = function(value) {
-  this.value = value;
-  this.next = null;
-};
+// remiveDups redone
+const LinkedList = require("../util/LinkedListX");
 
-/* FUNCTIONS */
-var checkDups = function(head, node) {
-  var currNode = head;
-  while (currNode !== node) {
-    if (currNode.value === node.value) {
-      return true;
+function removeDuplicates(list) {
+  const _set = new Set();
+  let cur = list.head;
+  let prev = null;
+  while (cur) {
+    if (_set.has(cur.value)) {
+      // duplicate found
+      // de-link it from the list
+      // cur jumps next but previous stays
+      // right behind cur (as always)
+      let elem = cur;
+      prev.next = cur.next;
+      cur = cur.next;
+      elem.next = null;
     }
-    currNode = currNode.next;
-  }
-  return false;
-};
-
-var printLinkedList = function(head) {
-  var node = head;
-  console.log('start of linked list');
-  while (node !== null) {
-    console.log(node.value);
-    node = node.next;
-  }
-  console.log('end of linked list');
-};
-
-var removeDups = function(head) {
-  var node = head;
-  while (node !== null) {
-    if (node.next !== null && checkDups(head, node.next)) {
-      node.next = node.next.next;
-    } else {
-      node = node.next;
+    else {
+      // add to the set
+      _set.add(cur.value);
+      prev = cur;
+      cur = cur.next;
     }
   }
-  return head;
-};
 
-/* TESTS */
-var a = new LinkedList('a');
-var b = new LinkedList('b');
-var c = new LinkedList('c');
-var d = new LinkedList('d');
-var e = new LinkedList('e');
+  return list;
+}
 
-a.next = b;
-b.next = c;
-c.next = d;
-d.next = e;
+// quick test
+let list = new LinkedList();
+for (let elem of [1, 5, 1, 6, 8, 6, 8, 8, 8, 8]) {
+  list.append(elem);
+}
 
-removeDups(a);
-printLinkedList(a);
+removeDuplicates(list);
 
-var f = new LinkedList('f');
-var g = new LinkedList('g');
-var h = new LinkedList('g');
-var i = new LinkedList('g');
-var j = new LinkedList('g');
-
-f.next = g;
-g.next = h;
-h.next = i;
-i.next = j;
-
-removeDups(f);
-printLinkedList(f);
-
-var k = new LinkedList('g');
-var l = new LinkedList('g');
-var m = new LinkedList('g');
-var n = new LinkedList('b');
-var o = new LinkedList('g');
-
-k.next = l;
-l.next = m;
-m.next = n;
-n.next = o;
-
-removeDups(k);
-printLinkedList(k);
+console.log(list._toArray()); // [1, 5, 6, 8]
---
+++ chapter02/util/LinkedListX.js
@@ -0,0 +1,95 @@
+// a new implementation of the linked list
+/*
+ADT:
+# Main operations
+prepend(value)        -> Add a node in the beginning
+append(value)         -> Add a node in the end
+pop()                 -> Remove a node from the end
+popFirst()            -> Remove a node from the beginning
+head()                -> Return the first node
+tail()                -> Return the last node
+remove(Node)*         -> Remove Node from the list
+*/
+
+// NOTE: no type-safety
+
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    append(value) {
+        let node = new Node(value);
+        // if list is empty
+        if (!this.head) {
+            this.head = node;
+            this.tail = node;
+        }
+        else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+    }
+
+    prepend(value) {
+        let node = new Node(value);
+        node.next = this.head;
+        this.head = node;
+    }
+
+    pop() {
+        let cur = this.head;
+
+        // only one or no item exists
+        if (!cur) return null;
+        if (!cur.next) {
+            this.head = null;
+            return cur;
+        }
+        // move till the 2nd last
+        while (cur.next.next)
+            cur = cur.next;
+        
+        let last = this.tail;
+        this.tail = cur;
+        this.tail.next = null;
+        return last;
+    }
+
+    popFirst() {
+        let first = this.head;
+        if (this.head && this.head.next) {
+            this.head = this.head.next;
+        }
+        return first;
+    }
+
+    head() {
+        return this.head;
+    }
+
+    tail() {
+        return this.tail;
+    }
+
+    _toArray() {
+        let arr = [];
+        let cur = this.head;
+        while (cur) {
+            arr.push(cur.value);
+            cur = cur.next;
+        }
+
+        return arr;
+    }
+}
+
+module.exports = LinkedList;
