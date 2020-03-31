ef3b611e7f281edeb887b025781c70feaa581ac1
Anthony Nandaa <profnandaa@gmail.com>
2020-03-31 19:54:13 +0300

    feat: add two more linkedlist methods (#63)
    - insertAt(index, value)
    - removeAt(index)

--- chapter03/util/LinkedList.js
+++ chapter03/util/LinkedList.js
@@ -65,6 +65,49 @@ class LinkedList {
       return this.head;
   }
 
+  removeAt(index) {
+    let i = 0;
+    let cur = this.head;
+    let prev = null;
+
+    while (cur != null) {
+      if (i == index) {
+        // remove
+        if (prev == null)
+          this.head = cur.next;
+        else prev.next = cur.next;
+        cur.next = null;
+        return cur.value;
+      }
+      else {
+        prev = cur;
+        cur = cur.next;
+        i++;
+      }
+    }
+    return null;
+  }
+
+  insertAt(index, value) {
+      if (index == 0) return this.prepend(value);
+      let cur = this.head;
+      let i = 0;
+
+      while (cur != null) {
+        if (i == index - 1) {
+            let node = new Node(value);
+            node.next = cur.next;
+            cur.next = node;
+            return true;
+        }
+        else {
+            i++;
+            cur = cur.next;
+        }
+      }
+      return false;
+  }
+
   tail() {
       return this.tail;
   }
@@ -82,3 +125,19 @@ class LinkedList {
 }
 
 module.exports = LinkedList;
+
+/* TEST */
+
+// let l = new LinkedList();
+// l.append(3);
+// l.append(4);
+// l.append(10);
+// l.append(20);
+// l.append(5);
+
+// console.log(l.removeAt(1), 4);
+// console.log(l.pop().value, 5);
+
+// console.log(l._toArray());
+// l.insertAt(2, 40);
+// console.log(l._toArray());
