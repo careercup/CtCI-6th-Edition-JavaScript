6c959a207be83bc99dbb99c2e433eb934043ee63
KevinLAnthony <kevinanthony79@yahoo.com>
2020-10-31 14:27:41 -0400

    Add files via upload

--- chapter01/1.3 - URLify/urlify.js
+++ chapter01/1.3 - URLify/urlify.js
@@ -1,24 +1,8 @@
 var urlify = function(str, length) {
-  // have a pointer to check from start to end
-  var strArr = str.split('');
-  var pointer = 0;
-  while (pointer < str.length) {
-    if (strArr[pointer] === ' ') {
-      // *** needs more work here, a little wierd
-      // not handling trailing spaces properly
-      for (var i = str.length - 1; i > pointer + 3; i--) {
-        strArr[i] = str[i - 2];
-      }
-      strArr[pointer] = '%';
-      strArr[pointer+1] = '2';
-      strArr[pointer+2] = '0';
-      console.log(strArr, strArr.length);
-    } 
-    pointer++;
-  }
-  // if character is a space, move remainder chars by two
-  // replace following three chars with '%20'
-  return strArr.join('');
+	return str.slice(0, length).split(' ').join('%20');
 };
 
 console.log(urlify('Mr John Smith    ', 13), 'Mr%20John%20Smith');
+console.log(urlify('', 0), '');
+console.log(urlify('1', 1), '1');
+console.log(urlify('1 0 ', 3), '1%200');
