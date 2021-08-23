2a072876ccaece406211799514f7aac48bf14a23
Damla Köksal <me@damlakoksal.com>
2021-08-23 18:06:54 +0300

    feat: add HashMap solution for question 1.2

--- chapter01/1.2 - Check Perm/checkPermute.js
+++ chapter01/1.2 - Check Perm/checkPermute.js
@@ -24,4 +24,31 @@ checkPermuteOneLine = (a, b) => {
     return (a.length === b.length && (a.split("").sort().join("") === b.split("").sort().join("")))
 }
 
-console.log(checkPermuteOneLine('aba', 'baa'));
+// Tests
+console.log(checkPermuteOneLine('aba', 'baa'), true);
+console.log(checkPermuteOneLine('aba', 'aa'), false);
+
+// HashMap Solution
+
+isPermutation = (a, b) => {
+
+    if (a.length !== b.length) return false;
+
+    let map = new Map();
+
+    for (let i = 0; i < a.length; i++) {
+        if (!map.get(a[i])) map.set(a[i], 0);
+        map.set(a[i], map.get(a[i]) + 1);
+    }
+    for (let i = 0; i < b.length; i++) {
+        map.set(b[i], map.get(b[i]) - 1);
+        if (map.get(b[i]) < 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPermutation("damla", "almad"), true);
+console.log(isPermutation("damla", "dmla"), false);
+console.log(isPermutation("damla", "daala"), false);
