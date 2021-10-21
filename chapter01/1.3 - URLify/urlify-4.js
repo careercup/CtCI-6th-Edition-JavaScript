78b896b2162f4668178bfb932a3a425cc7363fcf
j3nhan <j3nnynhan@gmail.com>
2021-10-20 17:16:39 -0700

    change the name

--- chapter01/1.3 - URLify/urlify-4.js
+++ chapter01/1.3 - URLify/urlify-4.js
@@ -1,6 +1,6 @@
 const urlify = (str) => {
-    const arr = str.trim().split(" ").join("%20")
-    return arr
+    const newStr = str.trim().split(" ").join("%20");
+    return newStr;
 }
 
 console.log(urlify('Mr John Smith    ', 13), 'Mr%20John%20Smith');
