c17a7e9f90250b7cc9b501f170dcf5aed9309a37
Hyewon Joo <hyewonjoo@Hyewons-MBP.lan>
2022-04-05 23:59:56 -0700

    added new test case to show another example of false result with same length of both strings

--- chapter01/1.2 - Check Perm/checkPermute.js
+++ chapter01/1.2 - Check Perm/checkPermute.js
@@ -14,6 +14,8 @@ var checkPermute = function(stringOne, stringTwo) {
 // Tests
 console.log(checkPermute('aba', 'aab'), true);
 
+console.log(checkPermute('aba', 'bba'), false);
+
 console.log(checkPermute('aba', 'aaba'), false);
 
 console.log(checkPermute('aba', 'aa'), false);
